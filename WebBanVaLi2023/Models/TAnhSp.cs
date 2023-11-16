9bf752e263026f4f81741d596d97024abd29042f
ndnnam <nhutnam249.gg@gmail.com>
2023-11-16 21:00:26 +0700

    Init Project

---
+++ WebBanVaLi2023/Models/TAnhSp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebBanVaLi2023.Models;
+
+public partial class TAnhSp
+{
+    public string MaSp { get; set; } = null!;
+
+    public string TenFileAnh { get; set; } = null!;
+
+    public short? ViTri { get; set; }
+
+    public virtual TDanhMucSp MaSpNavigation { get; set; } = null!;
+}
