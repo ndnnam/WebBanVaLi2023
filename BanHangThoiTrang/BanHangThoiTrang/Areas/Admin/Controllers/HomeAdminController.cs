030605fcc9a1d486a286a3ee952c8e199ec23f0d
anhtuanvo <anhtuanvo2209@gmail.com>
2023-11-19 14:08:02 +0700

    clip 11, 12

--- BanHangThoiTrang/BanHangThoiTrang/Areas/Admin/Controllers/HomeAdminController.cs
+++ BanHangThoiTrang/BanHangThoiTrang/Areas/Admin/Controllers/HomeAdminController.cs
@@ -1,6 +1,7 @@
 ﻿using Azure;
 using BanHangThoiTrang.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using X.PagedList;
 
@@ -32,6 +33,16 @@ namespace BanHangThoiTrang.Areas.Admin.Controllers
         [HttpGet]
         public IActionResult ThemSanPhamMoi()
         {
+            ViewBag.MaChatLieu = new SelectList(db.TChatLieus.ToList()
+                ,"MaChatLieu","ChatLieu");
+            ViewBag.MaHangSx = new SelectList(db.THangSxes.ToList()
+                , "MaHangSx", "HangSx");
+            ViewBag.MaNuocSx = new SelectList(db.TQuocGia.ToList()
+                , "MaNuoc", "TenNuoc");
+            ViewBag.MaLoai = new SelectList(db.TLoaiSps.ToList()
+                , "MaLoai", "Loai");
+            ViewBag.MaDt = new SelectList(db.TLoaiDts.ToList()
+                , "MaDt", "TenLoai");
             return View();
         }
         [Route("ThemSanPhamMoi")]
@@ -46,6 +57,62 @@ namespace BanHangThoiTrang.Areas.Admin.Controllers
                 return RedirectToAction("DanhMucSanPham");
             }
             return View(sanPham);
+        }
+
+        // =========== Sửa sản phẩm ===========
+        [Route("SuaSanPham")]
+        [HttpGet]
+        public IActionResult SuaSanPham(string maSanPham)
+        {
+            ViewBag.MaChatLieu = new SelectList(db.TChatLieus.ToList()
+                , "MaChatLieu", "ChatLieu");
+            ViewBag.MaHangSx = new SelectList(db.THangSxes.ToList()
+                , "MaHangSx", "HangSx");
+            ViewBag.MaNuocSx = new SelectList(db.TQuocGia.ToList()
+                , "MaNuoc", "TenNuoc");
+            ViewBag.MaLoai = new SelectList(db.TLoaiSps.ToList()
+                , "MaLoai", "Loai");
+            ViewBag.MaDt = new SelectList(db.TLoaiDts.ToList()
+                , "MaDt", "TenLoai");
+            var sanPham = db.TDanhMucSps.Find(maSanPham);
+            return View(sanPham); /*Tạo view SuaSanPham*/
+        }
+        [Route("SuaSanPham")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SuaSanPham(TDanhMucSp sanPham)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sanPham).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("DanhMucSanPham", "HomeAdmin");
+            }
+            return View(sanPham);
+        }
+
+        // =========== Xoá sản phẩm ===========
+        // Chỉ xoá sản phẩm trong trường hợp nhập nhầm dữ liệu khi thêm;
+        // Hoặc là sản phẩm chưa được đưa vào chi tiêt sản phẩm để bán;
+        [Route("XoaSanPham")]
+        [HttpGet]
+        public IActionResult XoaSanPham(string maSanPham)
+        {
+            TempData["Message"] = "";
+            //Tìm trong chi tiết sản phẩm, nếu có mã sp trùng khớp trong chi tiêt sp sẽ không thể xoá;
+            var chiTietSanPhams = db.TChiTietSanPhams.Where(x => x.MaSp == maSanPham).ToList();
+            if(chiTietSanPhams.Count() > 0)
+            {
+                TempData["Message"] = "Không xoá được sản phẩm này";
+                return RedirectToAction("DanhMucSanPham", "HomeAdmin");
+            }
+            // khi đã tìm thấy không có trong chi tiết sản phẩm sẽ tìm mã sản phẩm và thực hiện xoá sp;
+            var anhSanPhams = db.TAnhSps.Where(x => x.MaSp == maSanPham);
+            if (anhSanPhams.Any()) db.RemoveRange(anhSanPhams);
+            db.Remove(db.TDanhMucSps.Find(maSanPham));
+            db.SaveChanges();
+            TempData["Message"] = "Sản phẩm đã được xoá";
+            return RedirectToAction("DanhMucSanPham", "HomeAdmin");
         }
     }
 }
